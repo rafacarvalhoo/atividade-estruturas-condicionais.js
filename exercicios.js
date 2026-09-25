0fd2c0ce00e55b26ff17b46c747490bd348cabc6
Rafaella <rafacarvalhosilva@icloud.com>
2026-09-25 14:38:59 -0300

    atividade estruturas condicionais em js

---
+++ exercicios.js
@@ -0,0 +1,51 @@
+// Exercício 1 - Classificação de Temperatura
+let temperatura = 25;
+
+if (temperatura < 15) {
+    console.log("Muito frio");
+} else if (temperatura >= 15 && temperatura <= 20 ) {
+    console.log("Frio");
+} else if (temperatura >= 21 && temperatura <= 28) {
+    console.log("Agradável");
+} else if (temperatura > 28 ) {
+    console.log("Muito quente");
+}
+
+
+// Exercício 2 - Nota e Conceito 
+let nota = 10
+
+if (nota >= 9) {
+    console.log ("Conceito A");
+} else if (nota >= 7) {
+    console.log ("Conceito B");
+} else if (nota >= 5) {
+    console.log ("Conceito C");
+}else {
+    console.log ("Conceito D");
+}
+
+
+// Exercício 3 - Dia da Semana 
+let dia = 7
+
+if (dia == 1) {
+    console.log ("Domingo"); 
+} else if (dia == 2) {
+    console.log ("Segunda-feira");
+} else if (dia == 3) {
+    console.log ("Terça-feira");
+} else if (dia == 4) {
+    console.log ("Quarta-feira");
+} else if (dia == 5) {
+    console.log ("Quinta-feira");
+} else if (dia == 6) {
+    console.log ("Sexta-feira");
+}else if (dia == 7) {
+    console.log ("Sábado");
+} else {
+    console.log ("Dia Inválido")
+}
+
+
+// Desafio - Calculadora de IMC 
